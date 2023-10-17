578f3212d3849231d2390ab19bd9fdcc27e75851
Jon <jherrera@opentrends.net>
2023-10-17 15:44:51 +0200

    Search page displays functionallity conditionals

--- src/heroes/helpers/getHeroesByName.js
+++ src/heroes/helpers/getHeroesByName.js
@@ -1,3 +1,5 @@
+import { heroes } from "../data/heroes";
+
 export const getHeroesByName = ( name = '' ) => {
 
     name = name.toLocaleLowerCase().trim();
--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -13,13 +13,16 @@ export const SearchPage = () => {
     const { q = '' } = queryString.parse( location.search );
     const heroes = getHeroesByName( q );
 
+    const showSearch = (q.length === 0);
+    const showError = (q.length > 0) && heroes.length === 0;
+
     const { searchText, onInputChange } = useForm({
         searchText: q
     });
 
     const onSearchSubmit = ( event ) => {
         event.preventDefault();
-        if ( searchText.trim().length <= 1 ) return;
+        // if ( searchText.trim().length <= 1 ) return;
 
         navigate(`?q=${ searchText }`);
 
@@ -54,12 +57,21 @@ export const SearchPage = () => {
                     <h4>Results</h4>
                     <hr />
 
-                    <div className="alert alert-primary">
-                        Search a hero
+                    {/* {
+                        ( q === '' )
+                        ? <div className='alert alert-primary'>Search a hero!</div>
+                        : ( heroes.length === 0 ) 
+                        && <div className="alert alert-danger">No hero with <b>{ q }</b>!</div>
+                    } */}
+
+                    <div className='alert alert-primary animate__animated animate__fadeIn' 
+                        style={{ display: showSearch ? '' : 'none' }}>
+                        Search a hero!
                     </div>
 
-                    <div className="alert alert-danger">
-                        No hero with <b>{ q }</b>
+                    <div className='alert alert-danger animate__animated animate__fadeIn' 
+                        style={{ display: showError ? '' : 'none' }}>
+                        No hero with <b>{ q }</b>!
                     </div>
 
                     {
